80597076feff973d7dc8ca2051eef67cd046c6f3
S-codes14 <smlmnguni14@gmail.com>
2021-08-14 05:19:32 +0200

    edited

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,11 +9,12 @@ const Hero = (props) => (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          Welcome To <br />
-          My Personal Portfolio
+        {/* <span className="grey-color"> &lt;</span> */}
+          <span className="logo-name">Sibongmusa Lungelo</span>
+          {/* <span className="grey-color">/&gt;</span> */}
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        A developer who loves to transform ideas into reality using code. The path of a self-taught developer can be scary and intimidating. For me, getting into code turned out to be the right choice anyways: When the responsibility for your growth, career and learning sits 100% on your own shoulders, you also completely own your wins.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -9,6 +9,26 @@ export const LeftSection = styled.div`
 
     margin: 0 auto;
   }
+  .grey-color {
+    color: #868e96;
+  }
+
+.logo {
+  display: block;
+  flex: none;
+  float: left;
+  font-size: 1.5em;
+  text-decoration: none;
+  margin-top: 10px;
+  line-height: normal;
+}
+.logo-name {
+  font-family: "Agustina Regular";
+  font-weight: bold;
+  font-variant-ligatures: no-common-ligatures;
+  -webkit-font-variant-ligatures: no-common-ligatures;
+  padding: 0 10px;
+}
   @media ${(props) => props.theme.breakpoints.md} {
     width: 100%;
     display: flex;
