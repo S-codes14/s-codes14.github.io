5c22007814635b9337dd57878f46086cb12dc069
s-codes14 <smlmnguni14@gmail.com>
2022-05-31 18:29:51 +0200

    no longer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by Sibongumusa Lungelo")}
+          {emoji("Made with ❤️ by S-codes14")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Thank please contact me for any work full-time or part-time
